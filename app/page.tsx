96236737cb97b038f3ba083126244f74b22d15c1
Andrew Thomas <andrewt@gmail.com>
2025-02-26 16:32:16 -0500

    add screenshots to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -36,18 +36,28 @@ export default function LandingPage() {
               <p className="text-muted-foreground">
               Easily track and celebrate the small habits that add up to big change. Whether it&apos;s making your bed, doing a single stretch, or simply filling your water bottle in the morning—our intuitive interface helps you reinforce progress without the pressure of overwhelming goals. Build momentum, stay consistent, and feel accomplished every day.
               </p>
+              <div className="flex justify-center">
+                <img src="/habit.png" alt="Small Win" className="w-3/4" />
+              </div>
+              
             </div>
             <div className="space-y-4">
               <h3 className="text-xl font-semibold">Daily Habit Journal for Awareness</h3>
               <p className="text-muted-foreground">
               Keep track of your daily habits with a built-in journaling feature that helps you stay mindful of your routines. Reflect on your habits at the end of each day, recognize patterns, and gain insights into what&apos;s working. By reviewing your daily habits in one place, you can stay intentional and make meaningful progress over time.
               </p>
+              <div className="flex justify-center">
+                <img src="/journal.png" alt="Small Win" className="w-3/4" />
+              </div>
             </div>
             <div className="space-y-4">
               <h3 className="text-xl font-semibold">Judgment-Free Habit Tracking</h3>
               <p className="text-muted-foreground">
-              Habit Tracker is designed to support you, not stress you. There are no streaks, no guilt-inducing alerts—just a simple, flexible system that helps you build habits at your own pace. Forget the pressure of perfection. Aim lower, stay consistent, and keep moving forward in a way that feels right for you.
+              Habit Tracker is designed to support you, not stress you. There are no streaks, no guilt just a simple, flexible system that helps you build habits at your own pace. Forget the pressure of perfection. Acheive small wins, stay consistent, and keep moving forward in a way that feels right for you.
               </p>
+              <div className="flex justify-center">
+                <img src="/workout.avif" alt="Small Win" className="w-3/4" />
+              </div>
             </div>
           </div>
         </section>
