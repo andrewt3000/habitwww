5a5000170099f6bd38857b5e614124b21b094241
Andrew Thomas <andrewt@gmail.com>
2025-02-26 16:37:40 -0500

    rounded image corners

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function LandingPage() {
               Easily track and celebrate the small habits that add up to big change. Whether it&apos;s making your bed, doing a single stretch, or simply filling your water bottle in the morning—our intuitive interface helps you reinforce progress without the pressure of overwhelming goals. Build momentum, stay consistent, and feel accomplished every day.
               </p>
               <div className="flex justify-center">
-                <img src="/habit.png" alt="Small Win" className="w-3/4" />
+                <img src="/habit.png" alt="Small Win" className="w-3/4 rounded-lg" />
               </div>
               
             </div>
@@ -47,7 +47,7 @@ export default function LandingPage() {
               Keep track of your daily habits with a built-in journaling feature that helps you stay mindful of your routines. Reflect on your habits at the end of each day, recognize patterns, and gain insights into what&apos;s working. By reviewing your daily habits in one place, you can stay intentional and make meaningful progress over time.
               </p>
               <div className="flex justify-center">
-                <img src="/journal.png" alt="Small Win" className="w-3/4" />
+                <img src="/journal.png" alt="Small Win" className="w-3/4 rounded-lg" />
               </div>
             </div>
             <div className="space-y-4">
@@ -56,7 +56,7 @@ export default function LandingPage() {
               Habit Tracker is designed to support you, not stress you. There are no streaks, no guilt just a simple, flexible system that helps you build habits at your own pace. Forget the pressure of perfection. Acheive small wins, stay consistent, and keep moving forward in a way that feels right for you.
               </p>
               <div className="flex justify-center">
-                <img src="/workout.avif" alt="Small Win" className="w-3/4" />
+                <img src="/workout.avif" alt="Small Win" className="w-3/4 rounded-lg" />
               </div>
             </div>
           </div>
